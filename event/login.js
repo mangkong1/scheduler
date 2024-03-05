38909c8728da063b3509d01f1aa12041dd10d70f
Ubuntu <ubuntu@ip-172-31-42-231.ap-northeast-2.compute.internal>
2024-03-05 10:59:44 +0000

    Feat: add dummy data and login function

---
+++ event/login.js
@@ -0,0 +1,52 @@
+const arr = [
+  {
+    id: "xogud1111",
+    pw: "1234",
+    name: "name1111",
+    email: "xogud1111@gmail.com",
+    deaprtment: "planning",
+    rank: "manager",
+  },
+  {
+    id: "xogud2222",
+    pw: "1234",
+    name: "name2222",
+    email: "xogud2222@gmail.com",
+    deaprtment: "planning",
+    rank: "member",
+  },
+  {
+    id: "xogud3333",
+    pw: "1234",
+    name: "name3333",
+    email: "xogud3333@gmail.com",
+    deaprtment: "design",
+    rank: "manager",
+  },
+];
+
+// idValue와 pwValue를 전역변수로 외부에서 선언할 때 작동안함...왜?
+// find 메서드를 이용해 콜백함수를 각 요소에 대해 실행하고 조건이 맞으면 true 반환
+document.querySelector("#move_scheduler").addEventListener("click", () => {
+  const idValue = document.querySelector("#id_box").value;
+  const pwValue = document.querySelector("#pw_box").value;
+  const user = arr.find((user) => user.id === idValue && user.pw === pwValue);
+  if (user) {
+    location.href = "scheduler.jsp";
+  } else {
+    alert("아이디 혹은 비밀번호가 일치하지 않습니다");
+    location.reload();
+  }
+});
+
+document.querySelector("#move_find_id").addEventListener("click", () => {
+  location.href = "find_id.jsp";
+});
+
+document.querySelector("#move_find_pw").addEventListener("click", () => {
+  location.href = "find_pw.jsp";
+});
+
+document.querySelector("#move_signup").addEventListener("click", () => {
+  location.href = "signup.jsp";
+});
--- page/login.jsp
+++ page/login.jsp
@@ -11,14 +11,15 @@
     <main>
       <section>
         <img style="width:100px" src="../img/calender_logo.png" alt="로고" />
-        <input type="text" placeholder="아이디" />
-        <input type="password" placeholder="비밀번호" />
-        <input type="submit" value="로그인" />
-        <a href="">아이디를 잊으셨나요?</a>
-        <a href="">비밀번호를 잊으셨나요?</a>
+        <input id="id_box" type="text" placeholder="아이디" />
+        <input id="pw_box" type="password" placeholder="비밀번호" />
+        <input id="move_scheduler" type="submit" value="로그인" />
+        <a id='move_find_id'>아이디를 잊으셨나요?</a>
+        <a id='move_find_pw'>비밀번호를 잊으셨나요?</a>
         <hr>
-        <button href="">새 계정 만들기</button>
+        <button id='move_signup'>새 계정 만들기</button>
       </section>
     </main>
+    <script src="../event/login.js"></script>
   </body>
 </html>
