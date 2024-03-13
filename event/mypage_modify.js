164153a4bbd30ec3a40947c57ce5999673e22369
Ubuntu <ubuntu@ip-172-31-42-231.ap-northeast-2.compute.internal>
2024-03-13 06:26:49 +0000

    Refactor: add constraints for mypage modify

--- event/mypage_modify.js
+++ event/mypage_modify.js
@@ -1,60 +1,97 @@
-let arr = [
-  {
-    id: "xogud1111",
-    pw: "1234",
-    name: "name1111",
-    email: "xogud1111@gmail.com",
-    part: "디자인팀",
-    rank: "팀장",
-  },
-];
-
 const pwBox = document.querySelector("#pw_box");
-pwBox.value = arr[0].pw;
-
 const pwCheckBox = document.querySelector("#pw_check_box");
-pwCheckBox.value = arr[0].pw;
-
+const pwUsable = document.querySelector("#pw_usable");
+const pwUnusable = document.querySelector("#pw_unusable");
 const nameBox = document.querySelector("#name_box");
-nameBox.value = arr[0].name;
-
 const emailBox = document.querySelector("#email_box");
-emailBox.value = arr[0].email;
-
-const partBox = document.querySelector("#part_box");
-partBox.value = arr[0].part;
-
-const rankBox = document.querySelector("#rank_box");
-rankBox.value = arr[0].rank;
+const emailCheckBtn = document.querySelector("#email_check_btn");
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const partSelect = document.querySelector("#part_select");
+const rankSelect = document.querySelector("#rank_select");
+let idCheck = true;
+let pwCheck = true;
+let pwCorrectCheck = true;
+let nameCheck = true;
+let emailCheck = true;
 
 // 비밀번호 일치여부 메세지 출력
-document.querySelector("#pw_check_box").addEventListener("input", () => {
-  const pwValue = document.querySelector("#pw_box").value;
-  const pwCheck = document.querySelector("#pw_check_box").value;
-  if (pwValue === pwCheck) {
-    document.querySelector("#pw_usable").style.display = "block";
-    document.querySelector("#pw_unusable").style.display = "none";
+document.querySelectorAll(".input_pw_box").forEach((input) =>
+  input.addEventListener("input", () => {
+    if (pwBox.value === "") {
+      pwBox.style.border = "";
+      pwCheck = false;
+    } else if (pwBox.value.length < 8 || pwBox.value.length > 10) {
+      pwBox.style.border = "3px solid #e63812";
+      pwCheck = false;
+    } else {
+      pwBox.style.border = "3px solid #1d66ff";
+      pwCheck = true;
+    }
+
+    if (pwCheckBox.value === "") {
+      pwCheckBox.style.border = "";
+      pwUnusable.style.display = "none";
+      pwCorrectCheck = false;
+    } else if (pwBox.value === pwCheckBox.value) {
+      pwUsable.style.display = "block";
+      pwUnusable.style.display = "none";
+      pwCheckBox.style.border = "3px solid #1d66ff";
+      pwCorrectCheck = true;
+    } else {
+      pwUsable.style.display = "none";
+      pwUnusable.style.display = "block";
+      pwCheckBox.style.border = "3px solid #e63812";
+      pwCorrectCheck = false;
+    }
+  })
+);
+
+// 입력값 배열로 전달
+nameBox.addEventListener("input", () => {
+  if (nameBox.value === "") {
+    nameBox.style.border = "";
+    nameCheck = false;
+  } else if (nameBox.value.length < 2 || nameBox.value.length > 10) {
+    nameBox.style.border = "3px solid #e63812";
+    nameCheck = false;
   } else {
-    document.querySelector("#pw_unusable").style.display = "block";
-    document.querySelector("#pw_usable").style.display = "none";
+    nameBox.style.border = "3px solid #1d66ff";
+    nameCheck = true;
   }
 });
 
-// 입력값 배열로 전달
+emailBox.addEventListener("input", () => {
+  if (emailBox.value === "") {
+    emailBox.style.border = "";
+    emailCheckBtn.disabled = true;
+    emailCheckBtn.style.backgroundColor = "#dadde1";
+    emailCheck = false;
+  } else if (!emailRegex.test(emailBox.value)) {
+    emailBox.style.border = "3px solid #e63812";
+    emailCheckBtn.disabled = true;
+    emailCheckBtn.style.backgroundColor = "#dadde1";
+    emailCheck = false;
+  } else {
+    emailBox.style.border = "3px solid #1d66ff";
+    emailCheckBtn.disabled = false;
+    emailCheckBtn.style.backgroundColor = "#42b729";
+    emailCheck = true;
+  }
+});
+
 document.querySelector("#register_btn").addEventListener("click", () => {
-  const pwValue = document.querySelector("#pw_box").value;
-  const nameValue = document.querySelector("#name_box").value;
-  const emailValue = document.querySelector("#email_box").value;
-  const partValue = document.querySelector("#part_box").value;
-  const rankValue = document.querySelector("#rank_box").value;
-
-  arr[0].pw = pwValue;
-  arr[0].name = nameValue;
-  arr[0].email = emailValue;
-  arr[0].part = partValue;
-  arr[0].rank = rankValue;
-
-  console.log(arr);
+  if (pwCheck === false) {
+    alert("올바른 비밀번호를 입력해주세요");
+  } else if (pwCorrectCheck === false) {
+    alert("올바른 비밀번호를 입력해주세요");
+  } else if (nameCheck === false) {
+    alert("올바른 이름을 입력해주세요");
+  } else if (emailCheck === false) {
+    alert("올바른 이메일을 입력해주세요");
+  } else {
+    alert("내 정보가 수정되었습니다");
+    location.href = "login.jsp";
+  }
 });
 
 document.querySelector("#cancel_btn").addEventListener("click", () => {
--- page/mypage_modify.jsp
+++ page/mypage_modify.jsp
@@ -13,22 +13,35 @@
       <section id="mypage_modify_section">
         <h1 id="mypage_modify_title">마이페이지</h1>
 
-        <input class="input_box" id="pw_box" type="password" />
-        <input class="input_box" id="pw_check_box" type="password" />
-        <span id="pw_usable" style="display:none">비밀번호가 일치합니다</span>
-        <span id="pw_unusable" style="display:none">비밀번호가 일치하지 않습니다</span>
+        <div>
+          <input class="input_pw_box" id="pw_box" type="password" placeholder="비밀번호" />
+          <span class="constraint_message">비밀번호는 8~10자로 입력해주세요</span>
+        </div>
+
+        <div>
+          <input class="input_pw_box" id="pw_check_box" type="password" placeholder="비밀번호 확인" />
+          <span class="constraint_message" id="pw_usable">비밀번호가 일치합니다</span>
+          <span class="constraint_message" id="pw_unusable">비밀번호가 일치하지 않습니다</span>
+        </div>        
+
+        <div>
+          <input class="input_box" id="name_box" type="text" placeholder="이름" />
+          <span class="constraint_message">이름은 2~10자로 입력해주세요</span>
+        </div>
+
+        <div class="check_container">
+          <input class="input_box_small" id="email_box" type="text" placeholder="이메일" />
+          <input class="check_btn" id="email_check_btn" type="submit" value="중복확인" />
+        </div>
         
-        <input class="input_box" id="name_box" type="text" />
-        <input class="input_box" id="email_box" type="text" />
-        
-        <select class="select_box" id="part_box">
+        <select class="select_box" id="part_select">
           <%-- selected disabled hidden에서 필요한 속성만 꺼냄 --%>
           <option disabled>부서</option>
           <option>기획팀</option>
           <option>디자인팀</option>
         </select>
         
-        <select class="select_box" id="rank_box">
+        <select class="select_box" id="rank_select">
           <option disabled>직급</option>
           <option>팀장</option>
           <option>팀원</option>
